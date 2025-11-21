f91ca0a60f993bd5f720b48376d7f3f79bfc72d5
Your Bebo <elbebanymostafa@gmail.com>
2025-11-22 01:28:10 +0200

    removed relation between Product and PurchaseOrder, added new migration

--- WMS.DL/Entities/PurchaseOrder.cs
+++ WMS.DL/Entities/PurchaseOrder.cs
@@ -17,9 +17,6 @@ public class PurchaseOrder
 
     public IReadOnlyCollection<PurchaseOrderItem> POItems { get; set; } = new List<PurchaseOrderItem>();
 
-    public Product Product { get; set; } = null!;
-    public int ProductId { get; set; }
-
     public Warehouse Warehouse { get; set; } = null!;
     public int WarehouseId { get; set; }
     public int VendorId { get; set; }
