f7003e16b5c1f6b4f022acfde7107cd04c654834
mohamed_abozied <m7md7sn120m@gmail.com>
2025-11-27 14:20:37 +0200

    update branch

--- WMS.DL/Entities/_Common/BaseAuditableEntity.cs
+++ WMS.DL/Entities/_Common/BaseAuditableEntity.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace WMS.DAL.Entities._Common
 {
-    public class BaseAuditableEntity<TKey> : BaseEntity<TKey>
+    public interface IBaseAuditableEntity
+    {
+        public DateTime CreatedOn { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+    public class BaseAuditableEntity<TKey> : BaseEntity<TKey>, IBaseAuditableEntity
         where TKey : IEquatable<TKey>
     {
         public DateTime CreatedOn { get; set; }
