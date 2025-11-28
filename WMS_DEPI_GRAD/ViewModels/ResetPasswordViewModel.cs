56b983fa0d9970bbc7303865074ba7e1b27141ff
Sabry Elsawy <elsawysabry16@gmail.com>
2025-11-28 18:11:38 +0200

    edit forgotpassword and reset password

--- WMS_DEPI_GRAD/ViewModels/ResetPasswordViewModel.cs
+++ WMS_DEPI_GRAD/ViewModels/ResetPasswordViewModel.cs
@@ -4,12 +4,20 @@ namespace WMS_DEPI_GRAD.ViewModels
 {
     public class ResetPasswordViewModel
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Token { get; set; } = null!;
+
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Password is required")]
-        public string Password { get; set; }
+        public string Password { get; set; } = null!;
+
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Confirm Password is required")]
-        [Compare( nameof(Password), ErrorMessage = "Passwords do not match")]
-        public string ConfirmPassword { get; set; }
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = null!;
     }
 }
