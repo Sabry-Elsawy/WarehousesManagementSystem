dd642bf2edc6821a839f580c5fddd9d703b64122
mohamed_abozied <m7md7sn120m@gmail.com>
2025-12-04 14:48:02 +0200

    inventory

---
+++ WMS.BLL/DTOs/AddInventoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.BLL.DTOs;
+public class AddInventoryDto
+{
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public string Location { get; set; }
+    public string Status { get; set; }
+}
